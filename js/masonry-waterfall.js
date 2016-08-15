6fd05dc9c9e3300514f82a3de7cd881a900aa336
JeremyDu <jeremyduwork@gmail.com>
2016-08-15 16:17:46 +0800

    img 預載test

--- js/masonry-waterfall.js
+++ js/masonry-waterfall.js
@@ -2,15 +2,9 @@
 var $grid = $('.grid').masonry({
   itemSelector: '.grid-item',
   percentPosition: true,
-  columnWidth: '.grid-sizer',
-  animate:true
+  columnWidth: '.grid-sizer'
 });
 // layout Isotope after each image loads
-// $grid.imagesLoaded().progress( function() {
-//   $grid.masonry();
-// });
-$(function(){
-	$('.container-fluid').imagesLoaded(function () {
-    $grid();
-	});
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry();
 });
