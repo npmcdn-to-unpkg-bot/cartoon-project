d09fff704877719541dd9bad2e6b6b40ea8ed09e
vanitax <anger637976@gmail.com>
2016-08-16 16:54:15 +0800

    fixed imagesloader

--- js/masonry-waterfall.js
+++ js/masonry-waterfall.js
@@ -1,10 +1,26 @@
-//waterfall
-var $grid = $('.grid').masonry({
-  itemSelector: '.grid-item',
-  percentPosition: true,
-  columnWidth: '.grid-sizer'
-});
-// layout Isotope after each image loads
-$grid.imagesLoaded().progress( function() {
-  $grid.masonry();
-});
+/**
+* @Author: Roxy Chen <VanitaxMacbookPro>
+* @Date:   2016-08-16T16:53:31+08:00
+* @Last modified by:   VanitaxMacbookPro
+* @Last modified time: 2016-08-16T16:53:31+08:00
+* @License: Copyright (c) by Giftpack Inc. All Rights Reserved.
+*/
+
+
+
+$(document).ready(function(){
+
+  //waterfall
+  var $grid = $('.grid').masonry({
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    columnWidth: '.grid-sizer'
+  });
+
+
+  // layout Isotope after each image loads
+  $grid.imagesLoaded().progress( function() {
+    $grid.masonry();
+  });
+
+})
